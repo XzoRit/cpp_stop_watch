6a51b44edb6b85ed4186a778f8b9b036c574ce37
XzoRit <xzorit@hotmail.com>
2021-03-21 20:39:20 +0100

    add documentation

--- lib/lib/lib.hpp
+++ lib/lib/lib.hpp
@@ -11,6 +11,21 @@ namespace xzr
 {
 namespace chrono
 {
+inline namespace v1
+{
+/// \brief Tag type for starting a stopwatch on creation.
+struct auto_start_t
+{
+};
+/// \brief Stopwatch created with this value will start automatically.
+constexpr const auto_start_t auto_start{};
+} // namespace v1
+} // namespace chrono
+} // namespace xzr
+namespace xzr
+{
+namespace chrono
+{
 namespace impl
 {
 class stop_watch_state
@@ -27,79 +42,11 @@ class stop_watch_state
     {
         _running = false;
     }
-    bool is_running() const noexcept
+    bool running() const noexcept
     {
         return _running;
     }
 };
-} // namespace impl
-} // namespace chrono
-} // namespace xzr
-namespace xzr
-{
-namespace chrono
-{
-inline namespace v1
-{
-struct auto_start_t
-{
-};
-constexpr const auto_start_t auto_start{};
-template <class Clock>
-class basic_stop_watch : private impl::stop_watch_state
-{
-  private:
-    using clock = Clock;
-    using time_point = typename clock::time_point;
-
-  public:
-    using duration = typename clock::duration;
-
-  public:
-    basic_stop_watch() = default;
-    explicit basic_stop_watch(auto_start_t)
-    {
-        start();
-    }
-    void start()
-    {
-        if (is_running())
-            throw std::runtime_error{"start was called when the stopwatch was already running"};
-        _start = clock::now();
-        on_start();
-    }
-    void stop()
-    {
-        if (!is_running())
-            throw std::runtime_error{"stop was called when the stopwatch was not running"};
-        _elapsed = clock::now() - _start;
-        on_stop();
-    }
-    void reset() noexcept
-    {
-        if (is_running())
-            _start = clock::now();
-        _elapsed = duration::zero();
-    }
-    template <class Duration = duration>
-    Duration peek() const noexcept
-    {
-        if (is_running())
-            return std::chrono::duration_cast<Duration>(clock::now() - _start + _elapsed);
-        return std::chrono::duration_cast<Duration>(_elapsed);
-    }
-    bool running() const noexcept
-    {
-        return is_running();
-    }
-
-  private:
-    time_point _start{};
-    duration _elapsed{duration::zero()};
-};
-using stop_watch = basic_stop_watch<std::chrono::high_resolution_clock>;
-using stop_watch_with_steady_clock = basic_stop_watch<std::chrono::steady_clock>;
-using stop_watch_with_system_clock = basic_stop_watch<std::chrono::system_clock>;
 template <class StopWatch, class Func>
 class benchmark_t
 {
@@ -113,7 +60,7 @@ class benchmark_t
         : _func{std::move(func)}
     {
     }
-    duration measure(int iterations) const
+    duration measure(int iterations) noexcept
     {
         stop_watch watch{auto_start};
         for (int i{}; i < iterations; ++i)
@@ -124,18 +71,122 @@ class benchmark_t
     }
 
   private:
-    void invoke(std::true_type) const
+    void invoke(std::true_type) noexcept
     {
         boost::hof::apply(_func);
     }
-    void invoke(std::false_type) const
+    void invoke(std::false_type) noexcept
     {
         volatile auto res = boost::hof::apply(_func);
         static_cast<void>(res);
     }
     Func _func;
 };
-/// \brief Named parameter for the \ref benchmark function.
+} // namespace impl
+} // namespace chrono
+} // namespace xzr
+namespace xzr
+{
+namespace chrono
+{
+inline namespace v1
+{
+/// \brief Models a physical stopwatch to measure how much time elapsed between starting and stopping it.
+///
+/// The precision of this stopwatch depends on the one from the specified clock but a coarser precision can be specified
+/// on each call to \ref peek. To start the stopwatch you can either call start on a default constructed stopwatch or
+/// use \ref auto_start_t on construction for starting it immediately. After calling \ref stop the elapsed
+/// time can be retrieved witth \ref peek. It is ok to call \ref peek on a running stopwatch to get a snapshot of the
+/// elapsed time. \ref reset can be used to set the elapsed time to zero. This works on a running aswell as on a stopped
+/// stopwatch.
+///
+/// \note Calling start on a running stopwatch and stop on a stopped stopwatch is an error and throws.
+///
+/// @tparam Clock Specifies the source for measuring how much time elapsed. It has to model the [named requirement
+/// Clock](https://en.cppreference.com/w/cpp/named_req/Clock)
+template <class Clock>
+class basic_stop_watch : private impl::stop_watch_state
+{
+  private:
+    using clock = Clock;
+    using time_point = typename clock::time_point;
+
+  public:
+    /// \brief Precision of stopwatch.
+    using duration = typename clock::duration;
+
+  public:
+    basic_stop_watch() = default;
+    /// \brief Stopwatch is started immediately.
+    ///
+    /// \post running returns true.
+    explicit basic_stop_watch(auto_start_t)
+    {
+        start();
+    }
+    /// \brief Starts the stopwatch.
+    ///
+    /// \pre \ref is_running returns false.
+    /// \post \ref is_running returns true.
+    ///
+    /// \throws std::runtime_error If precondition is violated.
+    void start()
+    {
+        if (running())
+            throw std::runtime_error{"start was called when the stopwatch was already running"};
+        _start = clock::now();
+        on_start();
+    }
+    /// \brief Stops the stopwatch.
+    ///
+    /// \pre \ref is_running returns true.
+    /// \post \ref is_running returns false. Multiple calls to \ref peek return the same value.
+    ///
+    /// \throws std::runtime_error If precondition is violated.
+    void stop()
+    {
+        if (!running())
+            throw std::runtime_error{"stop was called when the stopwatch was not running"};
+        _elapsed = clock::now() - _start;
+        on_stop();
+    }
+    /// \brief Reset elapsed time to zero.
+    ///
+    /// Can be called on a stoppped aswell as on a running stopwatch.
+    void reset() noexcept
+    {
+        if (running())
+            _start = clock::now();
+        _elapsed = duration::zero();
+    }
+    /// \brief Returns the elapsed time since stopwatch was started.
+    ///
+    /// Can be called even if stopwatch is running to get a snapshot of the elapsed time since \ref start has been
+    /// called.
+    ///
+    /// \tparam Duration Precision to be returned.
+    template <class Duration = duration>
+    Duration peek() const noexcept
+    {
+        if (running())
+            return std::chrono::duration_cast<Duration>(clock::now() - _start + _elapsed);
+        return std::chrono::duration_cast<Duration>(_elapsed);
+    }
+    /// \brief True if stopwatch is running otherwise false.
+    bool is_running() const noexcept
+    {
+        return running();
+    }
+
+  private:
+    time_point _start{};
+    duration _elapsed{duration::zero()};
+};
+//// \brief Stopwatch using std::chrono::high_resolution_clock as a source.
+using stop_watch = basic_stop_watch<std::chrono::high_resolution_clock>;
+//// \brief Stopwatch using std::chrono::steady_clock as a source.
+using stop_watch_with_steady_clock = basic_stop_watch<std::chrono::steady_clock>;
+/// \brief Named parameter for the sepcifying the amount of iterations.
 class iterations
 {
   public:
@@ -155,13 +206,13 @@ class iterations
 };
 /// \brief Executes given function n-times and returns the execution time.
 ///
-/// @tparam Duration Precision of execution time.
-/// @tparam AutoStopwatch Defines the stopwatch to measure execution time with.
-template <class Duration = std::chrono::milliseconds, class StopWatch = stop_watch, class Func>
+/// @tparam Duration Precision of execution time to be returned.
+/// @tparam Stopwatch Defines the stopwatch to measure execution time with.
+template <class Duration = typename stop_watch::duration, class StopWatch = stop_watch, class Func>
 Duration benchmark(iterations n, Func&& func)
 {
     return std::chrono::duration_cast<Duration>(
-        benchmark_t<StopWatch, Func>{std::forward<Func>(func)}.measure(n.get()));
+        impl::benchmark_t<StopWatch, Func>{std::forward<Func>(func)}.measure(n.get()));
 }
 } // namespace v1
 } // namespace chrono
--- lib/lib/lib.test.cpp
+++ lib/lib/lib.test.cpp
@@ -4,7 +4,6 @@
 
 #include <chrono>
 #include <iostream>
-#include <thread>
 
 namespace std
 {
@@ -35,20 +34,20 @@ BOOST_AUTO_TEST_SUITE(test_stop_watch)
 BOOST_AUTO_TEST_CASE(not_running_on_construction)
 {
     xzr::chrono::stop_watch w{};
-    BOOST_TEST(!w.running());
+    BOOST_TEST(!w.is_running());
     BOOST_TEST(w.peek() == xzr::chrono::stop_watch::duration::zero());
 }
 BOOST_AUTO_TEST_CASE(construction_with_auto_start)
 {
     xzr::chrono::stop_watch w{xzr::chrono::auto_start};
-    BOOST_TEST(w.running());
+    BOOST_TEST(w.is_running());
     BOOST_TEST(w.peek() > xzr::chrono::stop_watch::duration::zero());
 }
 BOOST_AUTO_TEST_CASE(start)
 {
     xzr::chrono::stop_watch w{};
     w.start();
-    BOOST_TEST(w.running());
+    BOOST_TEST(w.is_running());
     const auto a{w.peek()};
     BOOST_TEST(a > xzr::chrono::stop_watch::duration::zero());
 }
@@ -56,7 +55,7 @@ BOOST_AUTO_TEST_CASE(stop)
 {
     xzr::chrono::stop_watch w{xzr::chrono::auto_start};
     w.stop();
-    BOOST_TEST(!w.running());
+    BOOST_TEST(!w.is_running());
     BOOST_TEST(w.peek() > xzr::chrono::stop_watch::duration::zero());
 }
 BOOST_AUTO_TEST_CASE(start_running_stop_watch)
@@ -85,6 +84,62 @@ BOOST_AUTO_TEST_CASE(reset_running_watch)
     const auto b{w.peek()};
     BOOST_TEST(b > xzr::chrono::stop_watch::duration::zero());
     BOOST_TEST(b < a);
+}
+BOOST_AUTO_TEST_CASE(peek)
+{
+    xzr::chrono::stop_watch w{};
+    const auto a{w.peek()};
+    BOOST_TEST(a == xzr::chrono::stop_watch::duration::zero());
+    w.start();
+    const auto b{w.peek()};
+    BOOST_TEST(b > a);
+    const auto c{w.peek()};
+    BOOST_TEST(c > b);
+    w.stop();
+    const auto d{w.peek()};
+    const auto e{w.peek()};
+    BOOST_TEST(d == e);
+    w.reset();
+    BOOST_TEST(w.peek() == xzr::chrono::stop_watch::duration::zero());
+}
+BOOST_AUTO_TEST_CASE(peek_with_duration)
+{
+    xzr::chrono::stop_watch w{};
+    static_assert(std::is_same<decltype(w.peek()), xzr::chrono::stop_watch::duration>::value, "");
+    static_assert(std::is_same<decltype(w.peek<std::chrono::milliseconds>()), std::chrono::milliseconds>::value, "");
+    static_assert(std::is_same<decltype(w.peek<std::chrono::hours>()), std::chrono::hours>::value, "");
+}
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE(test_benchmark)
+BOOST_AUTO_TEST_CASE(iteration)
+{
+    int a{};
+    xzr::chrono::benchmark(xzr::chrono::iterations{552}, [&a]() mutable { ++a; });
+    BOOST_TEST(a == 552);
+}
+BOOST_AUTO_TEST_CASE(void_function)
+{
+    xzr::chrono::benchmark(xzr::chrono::iterations{1}, []() {});
+}
+BOOST_AUTO_TEST_CASE(non_void_function)
+{
+    xzr::chrono::benchmark(xzr::chrono::iterations{1}, []() { return 552; });
+}
+BOOST_AUTO_TEST_CASE(uses_given_stop_watch)
+{
+    struct spy_stop_watch
+    {
+        using duration = std::chrono::microseconds;
+        explicit spy_stop_watch(xzr::chrono::auto_start_t)
+        {
+        }
+        duration peek() const noexcept
+        {
+            return duration{552};
+        }
+    };
+    BOOST_TEST((xzr::chrono::benchmark<spy_stop_watch::duration, spy_stop_watch>(xzr::chrono::iterations{1}, []() {
+               })) == spy_stop_watch::duration{552});
 }
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
